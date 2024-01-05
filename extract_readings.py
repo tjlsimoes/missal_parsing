4bd32251de5e9bbef365363dd4ff22b666f92283
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2024-01-05 11:43:36 +0000

    Focus on treatment of first reading and gospel

--- extract_readings.py
+++ extract_readings.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from bs4 import BeautifulSoup
+import re
 
 def recursive_defaultdict():
   return defaultdict(recursive_defaultdict)
@@ -175,26 +176,27 @@ def get_mass_by_sections(mass_raw_text, sections):
 
 # Output test:
 
-# possible_sections = [
-#     "ANTÍFONA DE ENTRADA",
-#     "ORAÇÃO COLECTA",
-#     "ANTÍFONA DA COMUNHÃO",
-#     "ORAÇÃO SOBRE AS OBLATAS",
-#     "ORAÇÃO DEPOIS DA COMUNHÃO",
-#     "LEITURA I ",
-#     "SALMO RESPONSORIAL",
-#     "ALELUIA",
-#     "LEITURA II",
-#     "EVANGELHO"
-# ]
+possible_sections = [
+    "ANTÍFONA DE ENTRADA",
+    "ORAÇÃO COLECTA",
+    "ANTÍFONA DA COMUNHÃO",
+    "ORAÇÃO SOBRE AS OBLATAS",
+    "ORAÇÃO DEPOIS DA COMUNHÃO",
+    "LEITURA I ",
+    "SALMO RESPONSORIAL",
+    "ALELUIA",
+    "LEITURA II",
+    "EVANGELHO"
+]
 
-# masses_raw_text = extract_sections("_old/AdvSem01.htm")
+masses_raw_text = extract_sections("_old/AdvSem01.htm")
 
-# masses_raw_text_init_key = list(masses_raw_text.keys())[1:][4]
+masses_raw_text_init_key = list(masses_raw_text.keys())[1:][0]
 
-# mass_raw_text = masses_raw_text[masses_raw_text_init_key]
+mass_raw_text = masses_raw_text[masses_raw_text_init_key]
 
-# print(repr(get_mass_by_sections(mass_raw_text, possible_sections).keys()))
+print(repr(get_mass_by_sections(mass_raw_text, possible_sections).keys()))
+print(repr(get_mass_by_sections(mass_raw_text, possible_sections)['EVANGELHO - Mt 24, 37-44']))
 
 
 def create_json_mass_readings(reading_idxs, mass_by_section):
@@ -213,7 +215,63 @@ def create_json_mass_readings(reading_idxs, mass_by_section):
       name = name.split(' ')[0].title() + ' ' + name.split(' ')[1].upper() 
       # Couldn't this be done more efficiently?
     reference = ' - '.join(data_from_title[1:])
-    # In case data_from_title contains more than one ' - ' substring
+    # In case data_from_title contains more than one ' - ' substring?
+
+    if reference == '':
+      reference = None
+
+    section_content = mass_by_section[sections[idx]]
+
+    reading_data = {}
+    reading_type = None
+
+    if 'Leitura' in name:
+      reading_type = 'reading-' + name.split(' ')[-1]
+      if reading_type in readings_present:
+        readings_present.append('alt-' + reading_type)
+        reading_type = f"alt-{reading_type}--{reading_type.count('alt-' + reading_type) + 1}"
+        # Shouldn't it be ....{reading_present.count('alt-' + reading_type)}....
+        # Will this count also readings signalled as, e.g., alt-reading I-1 ?
+      else:
+        readings_present.append(reading_type)
+      reading_data["reference"] = reference
+      base_idx = 0
+      if section_content[0][0] == '«':
+        reading_data['snippet'] = section_content[base_idx]
+      else:
+        base_idx = -1   # What is the need of this base_idx being set to -1?
+      reading_data["announcement"] = section_content[base_idx + 1]
+      reading_data['text'] = re.sub(r'(Palavra do Senhor\.)$', '', section_content[base_idx + 2])
+      # Why the parenthesis surrounding Palavra do Senhor\. ?
+      # ChatGPT:
+      # The purpose of the parentheses in the regular expression is to create a capturing group. In this specific case, it captures the entire string "Palavra do Senhor." at the end of the input string ($ asserts the position at the end of the string). This capturing group is not used in the replacement (since the replacement is an empty string), but it could be used if you wanted to refer to the captured text in the replacement string or elsewhere in your code.
+
+      # How does section_content[0 + 2] // section_content[-1 + 2] target p{Palavra do Senhor.} ?
+      # It doesn't. It pressuposes, 'Palavra do Senhor.' is included in the text section.
+      # Is this a logic that is most of the times applicable? For Terça, Quarta, Quinta Primeira
+      # Semana do Advento it is not applicable.
+
+    if 'Evangelho' in name:
+      reading_type = 'gospel'
+      reading_data['reference'] = reference
+      if section_content[0][0] == '«':
+        reading_data['snippet'] = section_content[base_idx]
+      else:
+        base_idx = -1
+        # Is there a need for this conditional new assignemnt of base_idx?
+      reading_data['snippet'] = section_content[0]
+      reading_data['announcement'] = section_content[1]
+      reading_data['text'] = re.sub(r"(Palavra da salvação\.)$", "", section_content[2])
+      # Again is this substitution required? For Primeira Semana Advento Domingo Ano A 
+      # e Quinta-Feira it was not.
+
+    
+    if reading_type != None:
+      readings[reading_type] = reading_data
+    else:
+      print('Reading type not recognized')
+
+  return readings
     
 
 
@@ -253,19 +311,19 @@ weekdays = ["1", "1", "1", "2", "3", "4", "5", "6", "7"]
 cycles = ["A", "B", "C"]
 
 
-for i, file_path in enumerate(file_paths):
-  masses_raw_text = extract_sections(file_path)
+# for i, file_path in enumerate(file_paths):
+#   masses_raw_text = extract_sections(file_path)
 
-  for i, key in enumerate(list(masses_raw_text.keys())[1:]):
-    # [1:] == Exclusion of first key in masses_raw_text as the 
-    #   first key refers to the first sunday section with its
-    #   propers.
-    mass_by_section = get_mass_by_sections(masses_raw_text[key], possible_sections)
-    sections = list(mass_by_section.keys())
+#   for i, key in enumerate(list(masses_raw_text.keys())[1:]):
+#     # [1:] == Exclusion of first key in masses_raw_text as the 
+#     #   first key refers to the first sunday section with its
+#     #   propers.
+#     mass_by_section = get_mass_by_sections(masses_raw_text[key], possible_sections)
+#     sections = list(mass_by_section.keys())
 
-    keywords = ["EVANGELHO", "LEITURA", "ALELUIA", "SALMO"]
-    reading_idxs = [i for i, element in enumerate(sections) if any(word in element for word in keywords)]
-    # CHATGPT: reading_idxs will contain the indices of elements in the sections list where any of the keywords are found.
-    # Is a dictionary an **ordered** collection of key-value pairs, as opposed to a unorderd collection of key-value pairs?
+#     keywords = ["EVANGELHO", "LEITURA", "ALELUIA", "SALMO"]
+#     reading_idxs = [i for i, element in enumerate(sections) if any(word in element for word in keywords)]
+#     # CHATGPT: reading_idxs will contain the indices of elements in the sections list where any of the keywords are found.
+#     # Is a dictionary an **ordered** collection of key-value pairs, as opposed to a unorderd collection of key-value pairs?
 
-    readings = create_json_mass_readings(reading_idxs, mass_by_section)
+#     readings = create_json_mass_readings(reading_idxs, mass_by_section)
